f8f85494395f848f141c8c97a2e1ed4e4eba287b
Michael Trew <mtrew2015@gmail.com>
2024-01-21 22:42:27 -0500

    hashing passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 // src/auth/auth.service.ts
-
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service'; // Import your UsersService
@@ -13,8 +13,10 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOneByUserId(username);
-    if (user && user.password === pass) {
-      const { ...result } = user;
+    console.log(user, 'user from validate');
+    if (user && bcrypt.compareSync(pass, user.password)) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
       return result;
     }
     return null;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'src/interfaces/user.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -15,11 +16,17 @@ export class UsersService {
   }
 
   async findOneByUserId(username: string): Promise<User | null> {
+    console.log(username, 'username from findOneByUserId');
+    username = username.toLowerCase();
     return this.userModel.findOne({ username }).exec();
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const userToLower = createUserDto.username.toLowerCase();
     const newUser = new this.userModel({
+      username: userToLower,
+      password: hashedPassword,
       ...createUserDto,
     });
     return newUser.save();
